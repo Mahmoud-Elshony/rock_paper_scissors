9c0092c289912996da545682d3448f1765cc6a4d
warda <mahmoud.elshony43@gmail.com>
2023-03-05 23:05:34 +0200

    make all files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let element = document.querySelectorAll('.choice');
+let result = document.querySelector('.result');
+// let allchoice = document.querySelector('.your-choice');
+let cumputerChoice = document.querySelector('.cumputer-choice');
+
+element.forEach((m) => {
+    m.style.backgroundImage = `url(img/${m.id}.jpg)`;
+    m.style.backgroundSize = '100px';
+    m.addEventListener('click', (d) => {
+        element.forEach(rem => {
+            if (rem.id != m.id) {
+                rem.remove()
+            }
+        })
+        m.style.pointerEvents = 'none';
+        let newComputerChoice = element[`${Math.floor(Math.random() * 2.5)}`].cloneNode();
+        cumputerChoice.append(newComputerChoice);
+
+        if (m.id == newComputerChoice.id) {
+            result.innerHTML = 'Drew!'
+        }
+
+        else if (
+            m.id == "paper" && newComputerChoice.id == "rock" ||
+            m.id == "scissors" && newComputerChoice.id == "paper" ||
+            m.id == "rock" && newComputerChoice.id == "scissors"
+        ) {
+            result.innerHTML = 'You Win!'
+            result.style.border = '5px solid green'
+        }
+        else {
+            result.innerHTML = 'You Loes!'
+            result.style.border = '5px solid red'
+        }
+    })
+
+})
