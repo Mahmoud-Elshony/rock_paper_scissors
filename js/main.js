ea60de45c30f0303317bbb8af2a9824c2e607a64
warda <mahmoud.elshony43@gmail.com>
2023-03-08 13:46:01 +0200

    add animate

--- js/main.js
+++ js/main.js
@@ -14,24 +14,40 @@ element.forEach((m) => {
         })
         m.style.pointerEvents = 'none';
         let newComputerChoice = element[`${Math.floor(Math.random() * 2.5)}`].cloneNode();
+        m.style.rotate = '180deg';
+        m.style.position = 'relative';
+        // m.style.top = '170px';
+        m.classList.add('active')
+        setTimeout(()=>{
+            if (m.id == newComputerChoice.id) {
+                result.innerHTML = 'Drew!'
+            }
+    
+            else if (
+                m.id == "paper" && newComputerChoice.id == "rock" ||
+                m.id == "scissors" && newComputerChoice.id == "paper" ||
+                m.id == "rock" && newComputerChoice.id == "scissors"
+            ) {
+                result.innerHTML = 'You Win!'
+                result.style.border = '5px solid green'
+            }
+            else {
+                result.innerHTML = 'You Loes!'
+                result.style.border = '5px solid red'
+            }
+        },1000);
         cumputerChoice.append(newComputerChoice);
 
-        if (m.id == newComputerChoice.id) {
-            result.innerHTML = 'Drew!'
-        }
-
-        else if (
-            m.id == "paper" && newComputerChoice.id == "rock" ||
-            m.id == "scissors" && newComputerChoice.id == "paper" ||
-            m.id == "rock" && newComputerChoice.id == "scissors"
-        ) {
-            result.innerHTML = 'You Win!'
-            result.style.border = '5px solid green'
-        }
-        else {
-            result.innerHTML = 'You Loes!'
-            result.style.border = '5px solid red'
-        }
+        
     })
 
 })
+// let list =['hello','ward','mido','ahmed'];
+// let choiceWord = list[1];
+// let newList = []
+
+// for(let i=0;i<choiceWord.length;i++){
+//     newList.push("_")
+// }
+// console.log(newList)
+// console.log(typeof newList)
